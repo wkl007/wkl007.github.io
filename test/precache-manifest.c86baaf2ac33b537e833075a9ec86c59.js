dcfb32b75a6f03d0d1996b3bd1a11ab2f0c74dde
wkl--007 <499657357@qq.com>
2018-12-05 15:16:35 +0800

    update

---
+++ test/precache-manifest.c86baaf2ac33b537e833075a9ec86c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffe9256f6f1fb657eee2a3fc7b55d45d",
+    "url": "/static/media/logo.ffe9256f.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86eca64ebd5f28a88c310aa956b74732",
+    "url": "/static/js/main.ff1dfc8d.chunk.js.gz"
+  },
+  {
+    "revision": "ff1dfc8d7196536053c1",
+    "url": "/static/js/main.ff1dfc8d.chunk.js"
+  },
+  {
+    "revision": "e8c065e3062374983194c7a17f60a531",
+    "url": "/static/js/1.77812815.chunk.js.gz"
+  },
+  {
+    "revision": "7781281579be9104b839",
+    "url": "/static/js/1.77812815.chunk.js"
+  },
+  {
+    "revision": "15d7ad1799f0661e9612df9fee058b1c",
+    "url": "/index.html"
+  }
+];
