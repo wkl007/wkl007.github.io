15c5cd42ac34307004c398d5e3503c65a2265ebb
wkl007 <499657357@qq.com>
2019-05-15 16:09:03 +0800

    feat: update react-ant-design

---
+++ react-ant-design/precache-manifest.3353d4bec19fae554a99cb7c956b65aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12ffdf8814592454b91d60a1fae74e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ef39bc2231b972560353",
+    "url": "./static/css/2.3eb1422d.chunk.css"
+  },
+  {
+    "revision": "ef39bc2231b972560353",
+    "url": "./static/js/2.6c153f2e.chunk.js"
+  },
+  {
+    "revision": "71763d13481c39b33968",
+    "url": "./static/js/main.9f31ed90.chunk.js"
+  },
+  {
+    "revision": "f92b8cd8f220ee1d09b8",
+    "url": "./static/js/runtime~main.a644bdce.js"
+  },
+  {
+    "revision": "ad7ff4ee88af008769cc52da90b87d17",
+    "url": "./static/js/vendor.2c253bd1.dll.js"
+  },
+  {
+    "revision": "ffe9256f6f1fb657eee2a3fc7b55d45d",
+    "url": "./static/media/logo.ffe9256f.svg"
+  }
+]);
